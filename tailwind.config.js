aec21765f3407df1ba4417b91d9bbf6236a599f1
Megan Bougle <81342403+meganbougle@users.noreply.github.com>
2024-11-13 01:02:48 -0600

    Version 2.1-New Navbar, font, and, icons

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#2997FF',
+        'custom-pink': '#CA054D',
+        'custom-blue-light': '#A0DDFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
